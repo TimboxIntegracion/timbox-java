4d5df76126997aeacf08797751a0c11a081e3f6a
Daniel Murillo <daniel.murillo@timbox.com.mx>
2017-11-07 15:11:31 -0600

    ADD metodo para generar sello y agregarlo al XML, para poder hacer pruebas funcionales

--- src/com/timbox/Main.java
+++ src/com/timbox/Main.java
@@ -1,9 +1,19 @@
 package com.timbox;
+import javax.xml.transform.*;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 import javax.xml.soap.SOAPMessage;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.io.*;
+import java.util.*;
+import java.text.SimpleDateFormat;
+import org.jdom2.*;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
 
 public class Main {
 
@@ -16,6 +26,7 @@ public class Main {
         // Timbrar Factura
         //
         // Parametros para el servicio
+        sellarXml("archivoXml.xml");
         byte[] archivoXml = Files.readAllBytes(Paths.get("archivoXml.xml"));
         String xmlBase64 = Base64.getEncoder().encodeToString(archivoXml);
 
@@ -51,5 +62,44 @@ public class Main {
         } catch (Exception exception) {
             throw exception;
         }
+    }
+    
+    public static void sellarXml(String xmlFile) throws Exception{
+        File inputFile = new File(xmlFile);
+        SAXBuilder saxBuilder = new SAXBuilder();
+        Document document = saxBuilder.build(inputFile);
+        //Obtener la fecha de ahorita y remplazarla en el atributo fecha
+        String timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(Calendar.getInstance().getTime());
+        timeStamp = timeStamp.replace('_','T');        
+        document.getRootElement().setAttribute("Fecha", timeStamp);
+        XMLOutputter xmlOutputter = new XMLOutputter(Format.getPrettyFormat());
+        xmlOutputter.output(document, new FileOutputStream(xmlFile));
+        
+        //Crear la cadena original
+        TransformerFactory factory = TransformerFactory.newInstance();
+        Source xslt;
+        xslt = new StreamSource(new File("cadenaoriginal_3_3.xslt"));
+        Transformer transformer = factory.newTransformer(xslt);
+        Source text;
+        text = new StreamSource(new File(xmlFile));
+        transformer.transform(text, new StreamResult(new File("cadena_original.txt")));
+        
+        //Crear digestion
+        String cmd = "openssl dgst -sha256 -sign CSD01_AAA010101AAA.key.pem -out digest.txt cadena_original.txt";
+        String[] cmdArray = cmd.split(" ");
+        Process p;
+        Runtime r=Runtime.getRuntime();
+        p=r.exec(cmd);
+        
+        //Generar Sello
+        cmd = "openssl enc -in digest.txt -out sello.txt -base64 -A -K CSD01_AAA010101AAA.key.pem";
+        cmdArray = cmd.split(" ");
+        p=r.exec(cmd);
+        
+        inputFile = new File(xmlFile);
+        document = saxBuilder.build(inputFile);
+        String sello = new Scanner(new File("sello.txt")).useDelimiter("\\Z").next();
+        document.getRootElement().setAttribute("Sello", sello);
+        xmlOutputter.output(document, new FileOutputStream(xmlFile));
     }
 }
